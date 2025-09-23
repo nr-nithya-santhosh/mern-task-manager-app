764d5061b182113ed30095249a7b0abb65709cf4
nsanthoshmaddula <nsanthoshmaddula@newrelic.com>
2025-09-23 17:55:13 +0530

    Added env file for FE to pick api url dynamically

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -7,9 +7,9 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-
+const API_URL = process.env.API_URL || 'http://localhost:5000';
   useEffect(() => {
-    axios.get('http://localhost:5000/api/tasks')
+    axios.get(`${API_URL}/api/tasks`)
       .then(response => {
         setTasks(response.data);
       })
@@ -20,7 +20,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/api/tasks', { title, description })
+    axios.post(`${API_URL}/api/tasks`, { title, description })
       .then(response => {
         setTasks([...tasks, response.data]);
         setTitle('');
